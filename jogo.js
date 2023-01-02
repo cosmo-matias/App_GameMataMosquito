b89df3c7aae04de5a322ecc76f176c7cec867095
Cosmo Matias <ducosmotdl@gmail.com>
2023-01-02 18:45:56 -0300

    Controlando os pontos de vida

--- jogo.js
+++ jogo.js
@@ -1,5 +1,6 @@
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
@@ -15,6 +16,15 @@ function posicaoRandomica() {
     //remover o mosquito anterior (caso exista)
     if(document.getElementById('mosquito')){
         document.getElementById('mosquito').remove()
+        
+        if(vidas > 3) {
+            alert('Interromper o jogo (game over)')
+        } else {
+            document.getElementById('v' + vidas).src="imagens/coracao_vazio.png"
+
+        vidas++
+        }
+        
     }
     
 
@@ -34,6 +44,9 @@ function posicaoRandomica() {
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.onclick = function() {
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
 
